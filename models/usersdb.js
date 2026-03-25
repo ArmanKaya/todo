72b03d1822a34a6a9453fa67d092fabc6a031064
ArmanKaya <armaniahhwastaken@gmail.com>
2026-03-25 15:17:59 +0100

    skole

--- models/usersdb.js
+++ models/usersdb.js
@@ -1,10 +1,11 @@
 const bcrypt = require("bcrypt")
 const sqlite = require("sqlite3");
 const db = new sqlite.Database("database.db");
+var jwt = require('jsonwebtoken');
 
 db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)")
 
-db.run("CREATE TABLE IF NOT EXISTS taskAccount (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGEr, task TEXT)")
+db.run("CREATE TABLE IF NOT EXISTS taskAccount (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, task TEXT)")
  
 
 
@@ -40,6 +41,7 @@ async function createAccount(userId) {
 }
 
 async function userExists(username) {
+    console.log(username, "fra db")
     return new Promise((resolve, reject) => {
         db.get("SELECT * FROM users WHERE name = ?", [username], (err, row) => {
             if (row) resolve(true)
@@ -48,8 +50,41 @@ async function userExists(username) {
     })
 }
 
+async function getUserByUsername(username) {
+    const username = req.body.username
+    return new Promise((resolve, reject) => {
+        db.get("SELECT * FROM users WHERE name = ?", [username], (err, row) => {
+            if (err) reject(err)
+            else resolve(row)
+        })
+    })
+}
+
+
+
+async function login(username, password) {
+    const user = (await getUserByUsername(username))
+    console.log(user)
+    if (!user) console.log("Brukernavn finnes ikke");
+    const passwordMatch = await new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+        });
+    });
+
+    if (!passwordMatch) throw new Error("Passordet er feil");
+
+    var token = jwt.sign({id: user.id, nickname: user.name, tasks: task}, 'shhhhh', {expiresIn: "10d"});
+    return token
+}
+
+
+
 module.exports = {
     createUser,
     createAccount,
-    userExists
+    userExists,
+    login,
+    getUserByUsername
 };
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var jwt = require('jsonwebtoken');
 const sqlite = require("sqlite3")
 const db = sqlite
 
-const { createUser, userExists, login, createAccount } = require("../models/usersdb")
+const { createUser, userExists, login, createAccount, getUserByUsername } = require("../models/usersdb")
 
 usersRouter.get("/profile", (req, res) => {
   res.render("profile")
@@ -16,7 +16,7 @@ usersRouter.get("/register", (req, res) => {
 });
 
 usersRouter.get("/login", (req, res) => {
-  res.render("register")
+  res.render("login")
 });
 
 usersRouter.post("/register", async (req, res) => {
@@ -28,5 +28,24 @@ usersRouter.post("/register", async (req, res) => {
     console.log(username, password)
     res.redirect("/users/login")
 });
+
+usersRouter.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    res.clearCookie("selected_account");
+
+try{
+    console.log(username, password)
+    const token = await login(username, password)
+    res.cookie("token", token, {maxAge: 1000 * 3600})
+    console.log(token)
+
+  }catch (err){
+        console.log("yay")
+     return res.status(400).render("login", { alert: "brukernavn eller passord er ikke gyldig"})  
+  }
+
+}
+
+);
 
 module.exports = { usersRouter }
